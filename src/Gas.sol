b5aac52e14e55fbdeeaa7586d8aaa9581677ef57
Escartin85 <scr85matrix@gmail.com>
2023-10-21 13:22:43 +0100

    Removing code not needed and optimized other lines

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,43 +2,35 @@
 pragma solidity 0.8.0;
 
 contract GasContract {
-    // Keeping the administrators array because one of the tests is getting they array items by index, still adding the mapping saves gas overall
-    address[5] public administrators;
-    mapping(address => bool) s_administrators;
-    uint256 totalSupply; // consider making this immutable
-    uint256 paymentCounter;
-    mapping(address => uint256) public balances;
-    mapping(address => Payment[]) payments;
-    mapping(address => uint256) public whitelist;
-    mapping(address => ImportantStruct) public whiteListStruct;
-    address contractOwner; // consider making this immutable
-    // @dev - 0x01 = tradeFlag, 0x02 = dividendFlag
-    uint8 constant FLAGS = 3;
-    uint8 constant TRADE_PERCENT = 12;
+    struct ImportantStruct {
+        bool paymentStatus;
+        uint256 amount;
+    }
 
     struct Payment {
         uint256 paymentID;
         uint256 amount;
     }
-
-    struct ImportantStruct {
-        uint256 amount;
-        bool paymentStatus;
-    }
+    
+    mapping(address => bool) s_administrators;
+    mapping(address => uint256) public balances;
+    //mapping(address => Payment[]) payments;
+    mapping(address => uint256) public whitelist;
+    mapping(address => ImportantStruct) public whiteListStruct;
+    
+    // Keeping the administrators array because one of the tests is getting they array items by index, still adding the mapping saves gas overall
+    address[5] public administrators;
+    address contractOwner; // consider making this immutable
+    //uint256 totalSupply; // consider making this immutable
+    //uint128 paymentCounter;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
 
-    function isAdminOrOwner() internal view {
-        address senderOfTx = msg.sender;
-        if (!s_administrators[senderOfTx] || senderOfTx != contractOwner) revert();
-    }
-
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
-        totalSupply = _totalSupply;
-        address senderOfTx = msg.sender;
-        balances[senderOfTx] = _totalSupply;
+        //balances[msg.sender] = totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
 
         assembly {
             mstore(32, s_administrators.slot)
@@ -54,24 +46,27 @@ contract GasContract {
         }
     }
 
+    /*function isAdminOrOwner() internal view {
+        if (!s_administrators[msg.sender] || msg.sender != contractOwner) revert();
+    }*/
+
     function balanceOf(address _user) external view returns (uint256 balance_) {
-        uint256 balance = balances[_user];
-        return balance;
+        return balances[_user];
     }
 
-    function getPayments(address _user) public view returns (Payment[] memory payments_) {
-        return payments[_user];
-    }
-
-    function transfer(address _recipient, uint256 _amount, string calldata _name) public returns (bool status_) {
+    function transfer(address _recipient, uint256 _amount, string calldata _name) external returns (bool status_) {
         // "I have the funds, trus me bro"
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-        payments[msg.sender].push(Payment({amount: _amount, paymentID: ++paymentCounter}));
+        //payments[msg.sender].push(Payment({amount: _amount, paymentID: ++paymentCounter}));
         return true;
+        /*assembly {
+            mstore(0, 1) // Store 'true' (1) in memory
+            return(0, 0x20) // Return 32 bytes of data from memory
+        }*/
     }
-
-    function updatePayment(address _user, uint256 _ID, uint256 _amount) public {
+/*
+    function updatePayment(address _user, uint256 _ID, uint256 _amount) external {
         isAdminOrOwner();
 
         for (uint256 ii = 0; ii < payments[_user].length; ii++) {
@@ -80,10 +75,13 @@ contract GasContract {
             }
         }
     }
-
-    function addToWhitelist(address _userAddrs, uint256 _tier) public {
-        isAdminOrOwner();
-        if (_tier >= 255) revert();
+*/
+    function addToWhitelist(address _userAddrs, uint256 _tier) external {
+        //isAdminOrOwner();
+        //if (!s_administrators[msg.sender] || msg.sender != contractOwner) revert();
+        //if (_tier >= 255) revert();
+        if ( (!s_administrators[msg.sender] || msg.sender != contractOwner) || (_tier >= 255) )revert();
+        
 
         assembly {
             let temp := 1
@@ -99,24 +97,28 @@ contract GasContract {
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
-    function whiteTransfer(address _recipient, uint256 _amount) public {
-        address senderOfTx = msg.sender;
+    function whiteTransfer(address _recipient, uint256 _amount) external {
+        //address senderOfTx = msg.sender;
+        whiteListStruct[msg.sender] = ImportantStruct(true, _amount);
+        
+        //uint256 senderBalance = balances[msg.sender];
+        //uint256 recipientBalance = balances[_recipient];
 
-        whiteListStruct[senderOfTx] = ImportantStruct(_amount, true);
-        uint256 senderBalance = balances[msg.sender];
-        uint256 recipientBalance = balances[_recipient];
-
-        senderBalance -= _amount;
-        recipientBalance += _amount;
+        //senderBalance -= _amount;
+        //recipientBalance += _amount;
 
         // Update sender and recipient balances
-        balances[msg.sender] = senderBalance + whitelist[msg.sender];
-        balances[_recipient] = recipientBalance - whitelist[msg.sender];
+        //balances[msg.sender] = senderBalance + whitelist[msg.sender];
+        //balances[_recipient] = recipientBalance - whitelist[msg.sender];
+        
+        balances[msg.sender] = ( balances[msg.sender] - _amount) + whitelist[msg.sender];
+        balances[_recipient] = ( balances[_recipient] + _amount) - whitelist[msg.sender];
 
         emit WhiteListTransfer(_recipient);
     }
 
-    function getPaymentStatus(address sender) public view returns (bool, uint256) {
+    function getPaymentStatus(address sender) external view returns (bool, uint256) {
         return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+
     }
 }
